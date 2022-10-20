4d2329cff3c157c0ae819337b48d0aaa484492ab
Ruanqin <teldrassildruid@gmail.com>
2022-10-20 02:21:13 -0400

    add static folder and stylesheet

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
+@app.route('/')
 def home(name=None):
     return render_template('index.html', name=name)
+
 
 # to run the app: $ flask --app server --debug run
